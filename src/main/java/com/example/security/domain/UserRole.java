6accfb56433faf7f213dcbe083239f02c84fcdef
TobeyKwon <tobeykwon@gmail.com>
2022-07-03 18:51:43 +0900

    사용자 객체 생성

---
+++ src/main/java/com/example/security/domain/UserRole.java
@@ -0,0 +1,15 @@
+package com.example.security.domain;
+
+public enum UserRole {
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+
+    private String roleName;
+
+    UserRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
